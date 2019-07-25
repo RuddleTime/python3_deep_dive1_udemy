95e76070aac0cc8196b25f0321a37968b2793d1b
Christina Ruddle <ruddlec@tcd.ie>
2019-07-25 14:50:41 -0400

    Adding script showing membory usage example for basic numerical python types:
    1. Integers
    2. Rational
    3. Real
    4. Complex
    5. Boolean

---
+++ 4_numeric_types/interger_types.py
@@ -0,0 +1,63 @@
+import sys
+import time
+
+integer = 100
+print("{0} is {1} and uses {2} memory".format(
+    integer,
+    type(integer),
+    sys.getsizeof(integer)  # Get size of memory used to store variable
+))
+
+integer = 0
+print("{0} is {1} and uses {2} memory".format(
+    integer,
+    type(integer),
+    sys.getsizeof(integer)  # Get size of memory used to store variable
+))
+
+rational = (1 / 2)
+print("{0} is {1} and uses {2} memory".format(
+    rational,
+    type(rational),
+    sys.getsizeof(rational)
+))
+
+real = 100.678
+print("{0} is {1} and uses {2} memory".format(
+    real,
+    type(real),
+    sys.getsizeof(real)
+))
+
+complex_num = complex(100, 2)
+print("{0} is {1} and uses {2} memory".format(
+    complex_num,
+    type(complex_num),
+    sys.getsizeof(complex_num)
+))
+
+boolean = True
+print("{0} is {1} and uses {2} memory".format(
+    boolean,
+    type(boolean),
+    sys.getsizeof(boolean)
+))
+
+# Confirming calculations with large numbers are slower
+def calc(a):
+    for i in range(10000000):
+        a * 2
+
+start = time.perf_counter()
+calc(10)
+end = time.perf_counter()
+print(end - start)
+
+start = time.perf_counter()
+calc(2**10000)
+end = time.perf_counter()
+print(end - start)
+
+
+
+
